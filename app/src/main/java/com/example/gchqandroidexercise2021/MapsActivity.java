babcb1bae54d4f0a078f896e44630f84154f035c
Alexis Caasi <alexiscaasi0@gmail.com>
2021-10-11 22:45:02 -0700

    We can get the user's location, display it, and drop a marker

--- app/src/main/java/com/example/gchqandroidexercise2021/MapsActivity.java
+++ app/src/main/java/com/example/gchqandroidexercise2021/MapsActivity.java
@@ -6,19 +6,33 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.Collections;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -26,15 +40,75 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     public static final int REQUEST_PERMISSIONS_LOCATION = 1;
 
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    private FusedLocationProviderClient fusedLocationClient;
+    public static final String TAG = "MapsActivity";
+
+    TextView latTV;
+    TextView lngTV;
+    Button markerButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        requestLocationPermission();
+
+        markerButton = findViewById(R.id.markerButton);
+        latTV = findViewById(R.id.textLat);
+        lngTV = findViewById(R.id.textLng);
+
+        markerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(v.getContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(v.getContext(),
+                                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    Log.i(TAG, "Check");
+                    return;
+                }
+                fusedLocationClient.getLastLocation()
+                        .addOnSuccessListener((Activity) v.getContext(), new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                LatLng userLocation = new LatLng(location.getLatitude(),location.getLongitude());
+                                mMap.addMarker(new MarkerOptions().position(userLocation).title("New Place"));
+                                mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+                            }
+                        });
+            }
+        });
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Check");
+            return;
+        }
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            Log.i(TAG, String.valueOf(location.getLongitude()));
+                            Log.i(TAG, String.valueOf(location.getLatitude()));
+                            Log.i(TAG, String.valueOf(location.getAltitude()));
+                            latTV.setText(location.getLatitude() + "");
+                            lngTV.setText(location.getLongitude() + "");
+                        } else {
+                            Log.e(TAG, "No location was found. Check if permissions is enabled....");
+                        }
+                    }
+                });
     }
 
     /**
@@ -49,18 +123,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
-        requestLocationPermission();
     }
 
     private void requestLocationPermission() {
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // No need to show rationale
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSIONS_LOCATION);
         }
     }
